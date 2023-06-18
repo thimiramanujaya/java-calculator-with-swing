eb8cd50c1453c89b0cccd1379171eca9503af96c
thimiramanujaya <128280023+thimiramanujaya@users.noreply.github.com>
2023-06-18 17:44:44 +0530

    implemented the function of subtrBtn

--- src/Calculator.java
+++ src/Calculator.java
@@ -200,6 +200,17 @@ public class Calculator extends javax.swing.JFrame {
         result.setText(String.valueOf(ans));
         
     }//GEN-LAST:event_plusBtnActionPerformed
+	
+	
+	private void subtrBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subtrBtnActionPerformed
+        
+        int num1 = Integer.parseInt(txtNum1.getText());
+        int num2 = Integer.parseInt(txtNum2.getText());
+        
+        int ans = num1 - num2;
+        
+        result.setText(String.valueOf(ans));
+    }//GEN-LAST:event_subtrBtnActionPerformed
 
 
     /**
